3220a892ec5cfc1809c84fcd3f330b33135c685a
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-10-16 01:48:30 +0600

    update user client

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,14 +1,36 @@
-import { useLoaderData } from "react-router-dom"
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const users = useLoaderData()
-    return (
-        <div>
-            <h1>{users.length}</h1>
-            {console.log(users.length)}
-            {users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)}
-        </div>
-    )
-}
+  const loaddedUsers = useLoaderData();
+  const [users, setUsers] = useState(loaddedUsers)
+  const handleDelete = (_id) => {
+    console.log(_id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if(data.deletedCount>0){
+            alert('deleted succesfully')
+            const remaining = users.filter(user => user._id !== _id)
+            setUsers(remaining)
+        }
+      });
+  };
+  return (
+    <div>
+      <h1>{users.length}</h1>
+      {users.map((user) => (
+        <p key={user._id}>
+          {user.name} : {user.email} id: {user._id}
+          <Link to={`update/${user._id}`}><button>Update</button></Link>
+          <button onClick={() => handleDelete(user._id)}>X</button>
+        </p>
+      ))}
+    </div>
+  );
+};
 
-export default Users
+export default Users;
